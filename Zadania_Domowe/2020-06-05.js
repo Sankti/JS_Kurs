2b849ae09105f5b912ea69a61143c8005f16996a
Adam <57396704+Sankti@users.noreply.github.com>
2020-06-05 19:59:22 +0200

    Update 2020-06-05.js

--- Zadania_Domowe/2020-06-05.js
+++ Zadania_Domowe/2020-06-05.js
@@ -10,9 +10,15 @@ let classes = [
 ];
 
 function dodajOsobe(classNo, name) {
-    properClass = classNo - 1
+    properClass = classNo - 1;
     classes[properClass].push(name);
     classes[properClass].sort();
+}
+
+function pobierzOsobe(classNo, studentNo) {
+    properClass = classNo - 1;
+    properStudentNo = studentNo - 1;
+    return classes[properClass][properStudentNo];
 }
 
 // Testing
@@ -20,3 +26,5 @@ dodajOsobe(2, "Charles")
 dodajOsobe(2, "Andrei")
 
 console.log(classes)
+
+console.log(pobierzOsobe(2, 1))
