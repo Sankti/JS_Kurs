812efd72fba6cee982d316d8fd3796fe6de8b023
Adam <57396704+Sankti@users.noreply.github.com>
2020-06-23 10:50:04 +0200

    Add files via upload

--- Zadania_z_Zajec/React_Components/App.js
+++ Zadania_z_Zajec/React_Components/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 import HelloWorld from './components/HelloWorld';
 import Clock from './components/clock';
-import {
-  zmienna,
-} from './App.const';
+
+import Message from './components/Messages';
+import Click from './components/Exercises/Click.js';
+import Picture from './components/Exercises/Picture.js';
+import Columns from './components/Exercises/Columns.js';
 
 function App() {
   return (
@@ -17,8 +19,12 @@ function App() {
         <p>
           <Clock/>
           Adam's App<br></br>
-          {zmienna}
         </p>
+        <Click defaultValue={10} />
+        <Click defaultValue={3} />
+        <Picture src="https://www.startpage.com/av/proxy-image?piurl=http%3A%2F%2Fusapears.org%2Fwp-content%2Fuploads%2F2007%2F11%2Fseckel-pear.jpg&sp=1592846841T279d9d55d4a7000105b6e2bdf1a175494a8f9928b17659342c8719f5d3936dc3" alt="Pear" />
+        <Message title="Tytuł 1" text="Paragraf pierwszy." />
+        <Columns />
         <a
           className="App-link"
           href="https://reactjs.org"
